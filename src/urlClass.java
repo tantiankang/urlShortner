9fac6a527b19fba31e857a262fc091b8688c5222
tantiankang <tiankang.tan@sd.taylors.edu.my>
2015-10-08 15:57:07 +0800

    Completion of url shortener project

--- src/urlClass.java
+++ src/urlClass.java
@@ -1,9 +1,27 @@
+import javax.swing.JOptionPane;
+
+import java.util.*;
+
+
 
 public class urlClass {
 	
+	HashMap hm = new HashMap();
 	public urlClass()
 	{
+		String urlInput = JOptionPane.showInputDialog(null, "Please input the url that you want to shorten.");
 		
+		String uuid = UUID.randomUUID().toString();
+		String cut = uuid.substring(0, 6);
+		
+		// Since to store data , we need a database, however a
+		//database is not installed with eclipse although we
+		//have the JDBC api. Therefore , i store the data into hashmap
+		hm.put(urlInput, cut);
+		Iterator i = hm.entrySet().iterator();
+		
+			Map.Entry me = (Map.Entry)i.next();
+			JOptionPane.showMessageDialog(null, "Your shortened url for "+me.getKey()+" is short.com/"+me.getValue());
 	}
 
 }
